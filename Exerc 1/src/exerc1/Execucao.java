6d5fd8d5f2d31f82c73563b888085d6af37620eb
gustavolopes2581 <gustavolester241@gmail.com>
2019-04-24 08:30:15 -0300

    Primeiro Commit

---
+++ Exerc 1/src/exerc1/Execucao.java
@@ -0,0 +1,14 @@
+package exerc1;
+
+import javax.swing.JOptionPane;
+
+public class Execucao {
+	
+	public static void main() {
+		Pessoa obj = new Pessoa();
+		String and = null, resp = null, dorm = null;
+		System.out.println(obj.Andar);
+		System.out.println(obj.Respirar);
+		System.out.println(obj.Dormir);
+	}
+}
---
+++ Exerc 1/src/exerc1/Pessoa.java
@@ -0,0 +1,21 @@
+package exerc1;
+
+import javax.swing.JOptionPane;
+
+public class Pessoa {
+	
+	public void Andar(String[] and){
+		and="Andando";
+		return;
+	}
+	
+	private void Dormir(String[] dorm){
+		dorm="Dormindo";
+		return;
+	}
+
+	protected void Respirar(String[] resp){
+		resp="Respirando";
+		return;
+	}
+}
